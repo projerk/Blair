8c0761a6940abcb926823dc46232d33ed2a4117e
Le Si Thai Son <sonle.25.lsts@gmail.com>
2024-11-21 20:56:33 +0700

    add constants

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constants {
+    private Constants() {
+
+    }
+
+    public static final String ACCENT_FONT_SRC = "src/main/resources/view/fonts/accent.ttf";
+    public static final String EXPLORE_VIEW_FILE = "ExploreView.fxml";
+    public static final String CANVAS_VIEW_FILE = "CanvasView.fxml";
+}
