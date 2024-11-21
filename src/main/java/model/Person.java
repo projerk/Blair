830b980315ca607fd45660238b2a5b1ada185191
Le Si Thai Son <sonle.25.lsts@gmail.com>
2024-11-21 08:11:06 +0700

    add file

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
