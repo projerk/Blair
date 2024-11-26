47cc68ec2c9a529802d8246347527109a6d93596
duon9 <ngducduong305@gmail.com>
2024-11-26 23:37:05 +0700

    add random utils

--- src/main/java/model/Authorization.java
+++ src/main/java/model/Authorization.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.Random;
+
+
 public class Authorization {
     private String username;
     private String password;
@@ -48,6 +51,26 @@ public class Authorization {
         System.out.println("HELLLO");
 
         return true;
+    }
+
+    public void doNothing() {
+        Random rand = new Random();
+  
+        // Generate random integers in range 0 to 999
+        int rand_int1 = rand.nextInt(1000);
+        int rand_int2 = rand.nextInt(1000);
+  
+        // Print random integers
+        System.out.println("Random Integers: "+rand_int1);
+        System.out.println("Random Integers: "+rand_int2);
+  
+        // Generate Random doubles
+        double rand_dub1 = rand.nextDouble();
+        double rand_dub2 = rand.nextDouble();
+  
+        // Print random doubles
+        System.out.println("Random Doubles: "+rand_dub1);
+        System.out.println("Random Doubles: "+rand_dub2);
     }
 
 }
