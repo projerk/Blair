4277fa953b7ba3d87422ae4da70603f74b1a0a61
pham nhat minh <phamnhatminh2k5@gmail.com>
2024-11-27 00:40:41 +0700

    update profie

--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -1,5 +1,25 @@
 package controller;
 
-public class ProfileController {
+import components.interfaces.Listener;
+import javafx.fxml.FXML;
+
+public class ProfileController implements Listener{
+
+    @FXML
+    private void initialize() {
+        
+    }
+
+    @Override
+    public void openCanvas(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'openCanvas'");
+    }
+
+    @Override
+    public void closeCanvas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'closeCanvas'");
+    }
 
 }
