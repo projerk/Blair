b4bc5526ea0ed3b63f412c8ff27b6ab5d417b2cf
pham nhat minh <phamnhatminh2k5@gmail.com>
2024-11-27 15:47:04 +0700

    more update in profile

--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -2,12 +2,33 @@ package controller;
 
 import components.interfaces.Listener;
 import javafx.fxml.FXML;
-
+import io.socket.emitter.Emitter;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import socket.SocketService;
+import javafx.application.Platform;
+import app.AppState;
 public class ProfileController implements Listener{
 
     @FXML
     private void initialize() {
-        
+        SocketService.getInstance().onMessage("user_response", (Emitter.Listener) args -> {
+            JSONObject response = (JSONObject) args[0];
+            String status = response.getString("status");
+            if (status.equals("success")) {
+                Platform.runLater(() -> {
+                    avatarPreload(response.getString("avatar"));
+                    bioPreload(response.getString("firstname"), response.getString("lastname"), response.getString("username"));
+                    bookReadPreload(response.getInt("readbook"), response.getInt("totalbook"));
+                    recentPreload(response.getJSONArray("recent"));
+                    lastPreload(response.getInt("last"));
+                });
+            }
+        });
+        //avatarPreload(null);
+        JSONObject data = new JSONObject();
+        data.put("user_id", AppState.getInstance().getUser().getID());
+        SocketService.getInstance().sendMessage("get_user", data);
     }
 
     @Override
