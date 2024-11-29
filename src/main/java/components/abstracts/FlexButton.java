4f851cb3112a5ce843dd960ec2ce2414c1ac0eeb
pham nhat minh <phamnhatminh2k5@gmail.com>
2024-11-29 16:56:09 +0700

    update button

--- src/main/java/components/abstracts/FlexButton.java
+++ src/main/java/components/abstracts/FlexButton.java
@@ -1,4 +1,12 @@
 package components.abstracts;
 
-public class FlexButton {
+import javafx.scene.control.Button;
+import socket.SocketService;
+public abstract class FlexButton extends Button {
+    protected SocketService client = SocketService.getInstance();
+
+    public FlexButton(boolean active) {
+        this.setStyle("-fx-font-size: 18; -fx-text-fill: white; -fx-font-family: 'Accent Graphic W00 Medium'; -fx-background-color: black; -fx-background-radius: 20px;");
+        // this.setDisable(!active);
+    }
 }
