49e849a9349b925451cff2d00dbabf1578ce11ee
pham nhat minh <phamnhatminh2k5@gmail.com>
2024-12-05 01:00:40 +0700

    update

--- src/main/java/utils/SocketUtils.java
+++ src/main/java/utils/SocketUtils.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import javafx.util.Pair;
 import model.Borrow;
 import model.User;
+import model.Borrow;
+import model.User;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.util.List;
@@ -128,25 +130,6 @@ public class SocketUtils {
         return res;
     }
 
-    public static List<Book> parseSearch(JSONArray books) {
-        ArrayList<Book> res = new ArrayList<>();
-
-        for (int i = 0; i < books.length(); i++) {
-            JSONObject object = books.getJSONObject(i);
-            Book book = new Book();
-            book.setId(object.getInt("id"));
-            book.setCover(object.getString("cover"));
-            book.setAuthor(object.getString("author"));
-            book.setTitle(object.getString("title"));
-            book.setGenre(object.getString("genre"));
-
-            res.add(book);
-        }
-
-        return res;
-    }
-
-
     public static List<Borrow> parseBorrowTableCell(JSONArray borrows) {
         List<Borrow> res = new ArrayList<>();
 
@@ -169,4 +152,3 @@ public class SocketUtils {
         return res;
     }
 }
-
