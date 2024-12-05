17d0ef33c21182acfe655a8d92f748b66ad9f5ce
pham nhat minh <phamnhatminh2k5@gmail.com>
2024-12-05 07:36:13 +0700

    update rate, star, pdfengine

---
+++ src/main/java/components/container/RatingField.java
@@ -0,0 +1,57 @@
+package components.container;
+
+import org.json.JSONObject;
+
+import components.button.StarButton;
+import components.interfaces.Receiver;
+import io.socket.emitter.Emitter;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import socket.SocketService;
+
+public class RatingField extends HBox implements Receiver {
+
+    public RatingField(int rating) {
+        this.setAlignment(Pos.CENTER_LEFT);
+        getMessage();
+        setSpacing(3);
+        for (int i = 1; i <= rating; i++) {
+            StarButton starButton = new StarButton(true);
+            starButton.setNumber(i);
+            getChildren().add(starButton);
+        }
+
+        for (int i = rating + 1; i <= 5; i++) {
+            StarButton starButton = new StarButton(false);
+            starButton.setNumber(i);
+            getChildren().add(starButton);
+        }
+    }
+
+    public void setRating(int rating) {
+        for (int i = 1; i <= rating; i++) {
+            StarButton starButton = (StarButton)getChildren().get(i - 1);
+            starButton.setActive(true);
+        }
+
+        for (int i = rating + 1; i <= 5; i++) {
+            StarButton starButton = (StarButton)getChildren().get(i - 1);
+            starButton.setActive(false);
+        }
+    }
+
+    public void getMessage() {
+        SocketService.getInstance().onMessage("rating_response", (Emitter.Listener) args -> {
+            JSONObject response = (JSONObject) args[0];
+            String status = response.getString("status");
+            if (status.equals("rating_success")) {
+                Platform.runLater(() -> {
+                    setRating(response.getInt("rating"));
+                });
+            }
+        });
+    }
+
+
+}
--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final String ACCENT_FONT_SRC = "src/main/resources/view/fonts/accent.ttf";
     public static final String EXPLORE_VIEW_FILE = "ExploreView.fxml";
     public static final String CANVAS_VIEW_FILE = "CanvasView.fxml";
+    public static final String URL = "http://127.0.0.1:5000";
 }
